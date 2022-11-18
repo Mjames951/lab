1296ace5ff79dd56c7aa33e1eb07f80a247da718
Mjames951 <aspc.james2@gmail.com>
2022-11-18 12:48:02 -0600

    added docstrings and type hinting

--- account.py
+++ account.py
@@ -1,24 +1,44 @@
 class Account:
-    def __init__(self, name):
-        self.__account_name = name
-        self.__account_balance = 0
+    def __init__(self, name: str) -> None:
+        '''
+        creates Account class with default balance: 0
+        :param name: name of the account
+        '''
+        self.__account_name: str = name
+        self.__account_balance: int = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: int) -> bool:
+        '''
+        Deposits money into account
+        :param amount: amount of money to be added into account
+        :return: boolean indicating success/failure of deposit
+        '''
         if amount<=0:
             return False
         else:
             self.__account_balance += amount
             return True
 
-    def withdraw(self, amount):
-        if amount>=0 or amount > self.__account_balance:
+    def withdraw(self, amount: int) -> bool:
+        '''
+        withdraws money from the account
+        :param amount: amount of money to be withdrawn from account
+        :return: boolean indicating succuss/failure of withdrawal
+        '''
+        if amount<=0 or amount > self.__account_balance:
             return False
         else:
             self.__account_balance -= amount
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> int:
+        '''
+        :return: balance of the account
+        '''
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> int:
+        '''
+        :return: name of the account
+        '''
         return self.__account_name
--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import pytest
 from account import *
 
-delta = 0.001
-
 class Test:
     def setup_method(self):
         self.account1 = Account('test1')
@@ -16,11 +14,28 @@ class Test:
         assert self.account2.get_name() == 'test2'
         assert self.account1.get_balance() == 0
         assert self.account2.get_balance() == 0
+    def test_deposit(self):
+        assert self.account1.deposit(10) == True
+        assert self.account2.deposit(-10) == False
+        assert self.account2.deposit(0) == False
+        assert self.account1.get_balance() == 10
+        assert self.account2.get_balance() == 0
+    def test_withdraw(self):
+        assert self.account1.deposit(10) == True
+        assert self.account1.get_balance() == 10
+        assert self.account1.withdraw(100) == False
+        assert self.account1.withdraw(5) == True
+        assert self.account2.withdraw(-5) == False
+        assert self.account2.withdraw(0) == False
+        assert self.account1.get_balance() == 5
+        assert self.account2.get_balance() == 0
 
 def main():
     T = Test
     T.setup_method()
     T.test_init()
+    T.test_deposit()
+    T.test_withdraw()
     T.teardown_method()
 
 
